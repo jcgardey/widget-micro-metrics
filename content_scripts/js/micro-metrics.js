2e563ae4e1670435a6d42c3df94b3f5777315240
juancruzgardey <juancruzgardey@hotmail.com>
2018-11-02 12:07:49 -0300

    capture metrics for Select Input

--- content_scripts/js/micro-metrics.js
+++ content_scripts/js/micro-metrics.js
@@ -1,71 +1,75 @@
-var capturerServerURL = "http://localhost:1701/metrics/";
-var focusTime;
-var typingLatency;
-var typingSpeed;
-var charsTyped = 0;
-var alreadyTyped = false;
-var typingIntervals = [];
-var lastKeypressTimestamp = 0;
-var charsDeleted = 0;
-
-
 //$("body").prepend("<div id='snackbar' style='z-index:1009;padding:20px;text-align:left;color:white;position:fixed;right:0;display:block;background:#333;display:none;'></div>");
+var capturerServerURL = "http://localhost:1701/metrics/";
 
-$("input").on('focus blur keypress keydown keyup', function (e) {
-    switch (e.type) {
-        case "focus":
-            focusTime = e.timeStamp;
-        case "keydown":
-            if (e.keyCode === 8) {
-                charsDeleted++;
-            }
-            break;
-        case "keypress":
-            // keypress is fired with printable characters
-            if (!alreadyTyped) {
-                typingLatency = e.timeStamp - focusTime;
-                alreadyTyped = true;
-            }
-            charsTyped++;
-            if (lastKeypressTimestamp != 0) {
-                var switchingTime = e.timeStamp;
-                var intraKeypressInterval = switchingTime - lastKeypressTimestamp;
-                typingIntervals.push(intraKeypressInterval);
-            }
-            lastKeypressTimestamp = e.timeStamp;
-            break;
-        case "blur":
-            totalTypingTime = e.timeStamp - (focusTime + typingLatency);
-            typingSpeed = totalTypingTime / charsTyped;
+function logMetrics(metrics) {
+    metrics["id"] = getRandomID();
+    metrics["timestamp"] = new Date().getTime();
+    $.post(capturerServerURL, metrics);
+    console.log(metrics);
+    //showSnackBar(metrics.id);
+    $("title").text(metrics.id);
+}
 
-            // if typingLatency is undefined the others metrics are undefined too
-            if (typingLatency) {
-                metrics = {
-                    "id": getRandomID(),
-                    "timestamp": new Date().getTime(),
-                    "typingLatency": typingLatency,
-                    "totalTypingTime": totalTypingTime,
-                    "typingSpeed": typingSpeed,
-                    "typingVariance": calculateVariance(typingIntervals),
-                    "typingIntervals": typingIntervals,
-                    "correctionAmount": charsDeleted
-                };
-                $.post( capturerServerURL, metrics );
-                console.log(metrics);
-                //showSnackBar(metrics.id);
-                $("title").text(metrics.id);
-            }
-            charsTyped = 0;
-            alreadyTyped = false;
-            typingIntervals = [];
-            lastKeypressTimestamp = 0;
-            charsDeleted = 0;
-            typingLatency = null;
-            break;
-        default:
-            null
-    }
-});
+    var focusTime;
+    var typingLatency;
+    var typingSpeed;
+    var charsTyped = 0;
+    var alreadyTyped = false;
+    var typingIntervals = [];
+    var lastKeypressTimestamp = 0;
+    var charsDeleted = 0;
+
+
+    $("input").on('focus blur keypress keydown keyup', function (e) {
+        switch (e.type) {
+            case "focus":
+                focusTime = e.timeStamp;
+            case "keydown":
+                if (e.keyCode === 8) {
+                    charsDeleted++;
+                }
+                break;
+            case "keypress":
+                // keypress is fired with printable characters
+                if (!alreadyTyped) {
+                    typingLatency = e.timeStamp - focusTime;
+                    alreadyTyped = true;
+                }
+                charsTyped++;
+                if (lastKeypressTimestamp != 0) {
+                    var switchingTime = e.timeStamp;
+                    var intraKeypressInterval = switchingTime - lastKeypressTimestamp;
+                    typingIntervals.push(intraKeypressInterval);
+                }
+                lastKeypressTimestamp = e.timeStamp;
+                break;
+            case "blur":
+                totalTypingTime = e.timeStamp - (focusTime + typingLatency);
+                typingSpeed = totalTypingTime / charsTyped;
+
+                // if typingLatency is undefined the others metrics are undefined too
+                if (typingLatency) {
+                    metrics = {
+                        "typingLatency": typingLatency,
+                        "totalTypingTime": totalTypingTime,
+                        "typingSpeed": typingSpeed,
+                        "typingVariance": calculateVariance(typingIntervals),
+                        "typingIntervals": typingIntervals,
+                        "correctionAmount": charsDeleted
+                    };
+                    logMetrics(metrics);
+                }
+                charsTyped = 0;
+                alreadyTyped = false;
+                typingIntervals = [];
+                lastKeypressTimestamp = 0;
+                charsDeleted = 0;
+                typingLatency = null;
+                break;
+            default:
+                null
+        }
+    });
 
 function calculateVariance(intervals) {
     var total = 0;
@@ -92,48 +96,46 @@ function showSnackBar(aMessage) {
 }
 
 
-//SelectMetrics();
-
-
 function SelectMetrics() {
     var clicks = 0;
     var keystrokes = 0;
     var optionsSelected = 0;
     var focusTime;
     var openTime;
+    var optionsDisplayTime = 0;
 
     $("select").on("focus", function (e) {
         focusTime = e.timeStamp;
     });
 
-    // triggered when options box is opened
-    $("select").on("mousedown", function () {
+    // triggered only when options box is opened
+    $("select").on("mousedown", function (e) {
         openTime = e.timeStamp;
         clicks++;
-        console.log("clicks " + clicks);
-    });
-
-    // only triggered when options is closed
-    $("select").on("keypress", function () {
-        console.log("keypress");
-        keystrokes++;
     });
 
 
-    $("select").on("change", function () {
-
+    $("select").on("change", function (e) {
         optionsSelected++;
+        if (openTime) {
+            optionsDisplayTime += e.timeStamp - openTime;
+        }
+    });
+
+    // only triggered when options box is closed
+    $("select").on("keypress", function () {
+        keystrokes++;
     });
 
     $("select").on("blur", function (e) {
         var now = e.timeStamp;
-        console.log({"clicks": clicks, "keystrokes": keystrokes, "optionsSelected": optionsSelected,
-            "focusTime": now - focusTime});
+        logMetrics({"clicks": clicks, "keystrokes": keystrokes, "optionsSelected": optionsSelected,
+            "focusTime": now - focusTime, "optionsDisplayTime": optionsDisplayTime});
         clicks = 0;
         keystrokes = 0;
         optionsSelected = 0;
-        focusTime = now;
+        optionsDisplayTime = 0;
     });
-
-
 }
+
+SelectMetrics();
