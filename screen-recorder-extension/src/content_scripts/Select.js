d646eee6d9047ca404eee179c6fe503a1081fc1b
juancruzgardey <juancruzgardey@hotmail.com>
2020-05-17 21:45:38 -0300

    native select boxes are replaced only when the target page is loaded to avoid layout disruptions

--- screen-recorder-extension/src/content_scripts/Select.js
+++ screen-recorder-extension/src/content_scripts/Select.js
@@ -1,18 +1,23 @@
-class Select {
+class CustomSelect {
 
-    constructor(element, originalSelect) {
-        this.dropdown = element;
-        this.menu = element.querySelector(".menu");
-        this.title = element.querySelector(".title");
+    constructor(originalSelect) {
         this.originalSelect = originalSelect;
-
-        this.menu.style.width = window.getComputedStyle(this.title).getPropertyValue("width");
+        this.createSelectWrapper();
         this.toggleMenuDisplay = this.toggleMenuDisplay.bind(this);
         this.handleOptionSelected = this.handleOptionSelected.bind(this);
+        this.closeMenuWhenClickingOut = this.closeMenuWhenClickingOut.bind(this);
         if (this.getOriginalSelectActiveOption()) {
             this.title.textContent = this.getOriginalSelectActiveOption().textContent;
         }
+        this.title.textContent = this.getOriginalSelectActiveOption() ? this.getOriginalSelectActiveOption().textContent:
+            this.getOriginalSelectOptions()[0].label;
         this.title.addEventListener("click", this.toggleMenuDisplay);
+        document.addEventListener("click", this.closeMenuWhenClickingOut);
+        const me = this;
+        this.dropdown.addEventListener("change", function () {
+            me.originalSelect.value = me.title.getAttribute("value");
+            me.originalSelect.dispatchEvent(new Event('change'));
+        });
     }
 
     getOriginalSelectOptions() {
@@ -26,7 +31,6 @@ class Select {
         while (this.menu.firstChild) {
             this.menu.removeChild(this.menu.lastChild);
         }
-
         const me = this;
         this.getOriginalSelectOptions().forEach(option => {
             var optionElement = document.createElement("div");
@@ -77,5 +81,70 @@ class Select {
 
     getOriginalSelectActiveOption() {
         return this.originalSelect.querySelector("option[selected]");
+    }
+
+    isMenuOpened() {
+        return this.menu.style.display == "";
+    }
+
+    closeMenuWhenClickingOut(event) {
+        if (event.target == this.dropdown || this.dropdown.contains(event.target)) {
+            return
+        }
+        if (this.isMenuOpened()) {
+            this.toggleMenuStyle();
+        }
+    }
+
+    createSelectWrapper() {
+        this.dropdown = document.createElement("div");
+        this.dropdown.setAttribute("class", "dropdown");
+        this.dropdown.setAttribute("widget-type","select");
+
+        this.title = document.createElement("div");
+        this.title.setAttribute("class", "title pointerCursor");
+        this.dropdown.appendChild(this.title);
+
+        this.menu = document.createElement("div");
+        this.menu.setAttribute("class", "menu pointerCursor");
+        this.menu.style.display = "none";
+        this.dropdown.appendChild(this.menu);
+
+        this.dropdown.style.fontSize = window.getComputedStyle(this.originalSelect).getPropertyValue("font-size");
+        this.dropdown.style.display = window.getComputedStyle(this.originalSelect).getPropertyValue("display");
+        this.dropdown.style.fontFamily = window.getComputedStyle(this.originalSelect).getPropertyValue("font-family");
+        this.dropdown.style.backgroundColor = window.getComputedStyle(this.originalSelect).getPropertyValue("background-color");
+        this.dropdown.style.color = window.getComputedStyle(this.originalSelect).getPropertyValue("color");
+        //this.dropdown.style.border = window.getComputedStyle(this.originalSelect).getPropertyValue("border");
+        this.dropdown.style.borderRadius = window.getComputedStyle(this.originalSelect).getPropertyValue("border-radius");
+        this.dropdown.style.margin = window.getComputedStyle(this.originalSelect).getPropertyValue("margin");
+
+
+        this.title.style.textAlign = window.getComputedStyle(this.originalSelect).getPropertyValue("text-align");
+        this.title.style.width = window.getComputedStyle(this.originalSelect).getPropertyValue("width");
+        this.title.style.height = window.getComputedStyle(this.originalSelect).getPropertyValue("height");
+        this.title.style.padding = window.getComputedStyle(this.originalSelect).getPropertyValue("padding");
+        this.title.style.border = window.getComputedStyle(this.originalSelect).getPropertyValue("border");
+        this.title.style.background = "url('https://selfrefactoring.s3.amazonaws.com/testsites/arrow-down.svg') right 10px center no-repeat";
+
+        if (window.getComputedStyle(this.originalSelect).getPropertyValue("background-color") != "rgba(0, 0, 0, 0)") {
+            this.menu.style.backgroundColor = window.getComputedStyle(this.originalSelect).getPropertyValue("background-color");
+        }
+        else {
+            this.menu.style.backgroundColor = "#ffff";
+        }
+        this.menu.style.width = window.getComputedStyle(this.originalSelect).getPropertyValue("width");
+        this.menu.style.color = window.getComputedStyle(this.originalSelect).getPropertyValue("color");
+        this.menu.style.border = window.getComputedStyle(this.originalSelect).getPropertyValue("border");
+
+        this.menu.style.zIndex = "9999";
+        this.menu.style.position = "absolute";
+        this.menu.style.borderRadius = ".4em";
+        this.menu.style.overflowX = "hidden";
+        this.menu.style.overflowY = "auto";
+        this.menu.style.maxHeight = "300px";
+
+        this.originalSelect.parentNode.insertBefore(this.dropdown, this.originalSelect);
+        this.originalSelect.style.display = "none";
     }
 }
