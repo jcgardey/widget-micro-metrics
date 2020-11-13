07131cfcf4ac269f6a9ef5fb5baf70d60c1eddc4
juancruzgardey <juancruzgardey@hotmail.com>
2020-11-13 15:38:27 -0300

    add new metrics: widgetbox (all widgets), optionsCount (radios and select) and enteredText(text inputs)

--- screen-recorder-extension/src/content_scripts/RadioGroup.js
+++ screen-recorder-extension/src/content_scripts/RadioGroup.js
@@ -9,12 +9,16 @@ class RadioGroup extends WidgetGroup {
 
     getLabel() {
         let label = '';
-        this.elements.filter(elem => { return elem.tagName.toLowerCase() == "label"}).map(elem => {
+        this.elements.filter(elem => elem.tagName.toLowerCase() == "label").map(elem => {
             let text = elem.textContent.trim().replace(/(\r\n|\n|\r)/gm, "");
             let elementLabel = text.length > 6 ? text.substring(0,5) : text;
             label += elementLabel + " ";
         });
         return label;
+    }
+
+    getRadiosCount() {
+        return this.elements.filter(elem => (elem.type == "radio" || elem.getAttribute("role") == "radio")).length;
     }
 
 
